f7ea04bee39cba5e9a05bd200dfebd7a6f44cd21
Valter-efp <154928977+Valter-efp@users.noreply.github.com>
2024-08-28 09:57:55 +0100

    Update ft_atoi_base.c
    Updates get_digit for more simple version

--- Exam Rank 2/Lvl2/ft_atoi_base/ft_atoi_base.c
+++ Exam Rank 2/Lvl2/ft_atoi_base/ft_atoi_base.c
@@ -7,19 +7,18 @@ char ft_tolower(char c)
 
 int get_digit(char c, int digits_in_base)
 {
-	int max_digit;
-
-	if (digits_in_base <= 10)
-		max_digit = digits_in_base - 1 + '0';
-	else
-		max_digit = digits_in_base -10 - 1 + 'a';
+	int digit;
 	
-	if (c >= '0' && c <= '9' && c <= max_digit)
-		return(c - '0');
-	else if (c >= 'a' && c <= 'f' && c <= max_digit)
-		return(10 + c - 'a');
+	if (c >= '0' && c <= '9')
+		digit = c - '0';
+	else if (c >= 'a' && c <= 'f')
+		digit = c - 'a' + 10;
 	else
-		return(-1);
+		return (-1);
+
+	if (digit >= digits_in_base)
+		return (-1);
+	return (digit);
 }
 
 int	ft_atoi_base(const char *str, int str_base)
