fa8ccf3814a392c7a03ba6d9fbbd04ad0da11838
Valter-efp <154928977+Valter-efp@users.noreply.github.com>
2024-09-08 16:03:49 +0100

    Add files via upload

---
+++ Exam Rank 2/Lvl2/print_hex/print_hex.c
@@ -0,0 +1,32 @@
+# include <unistd.h>
+
+int ft_atoi(char *str)
+{
+	int nbr = 0;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		nbr = nbr * 10;
+		nbr = nbr + str[i] - '0';
+		++i;
+	}
+	return (nbr);
+}
+
+void print_hex(int nbr)
+{
+	char hex[] = "0123456789abcdef";
+
+	if (nbr >= 16)
+		print_hex(nbr / 16);
+	write(1, &hex[nbr % 16], 1);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+		print_hex(ft_atoi(argv[1]));
+	write (1, "\n", 1);
+	return (0);	
+}
