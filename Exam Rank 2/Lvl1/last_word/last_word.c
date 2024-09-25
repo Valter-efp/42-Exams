cdda8e41c600ba83de7af15ad1179153add64f60
Valter-efp <154928977+Valter-efp@users.noreply.github.com>
2024-09-25 13:16:13 +0100

    Update last_word.c

--- Exam Rank 2/Lvl1/last_word/last_word.c
+++ Exam Rank 2/Lvl1/last_word/last_word.c
@@ -1,28 +1,34 @@
-#include <unistd.h>
+# include <unistd.h>
 
-int main (int argc, char **argv)
+void ft_putword(char *str, int ws, int we)
 {
-	int len = 0;
+	while (ws < we)
+	{
+		write(1, &str[ws], 1);
+		ws++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	int i = 0;
 	int lwe = 0;
 	int lws = 0;
 
 	if (argc == 2)
 	{
-		while(argv[1][len])
-			len++;
-		lwe = len - 1;
-		while(lwe >= 0 && (argv[1][lwe] == ' ' || argv[1][lwe] == '\t'))
-			lwe--;
-		lws = lwe;
-		while(lws >= 0 && (argv[1][lws] != ' ' && argv[1][lws] != '\t'))
-			lws--;
-		lws++;
-			while(lws <= lwe )
-		{
-			write(1, &argv[1][lws], 1);
-			lws++;
-		}
+		char *str = argv[1];
+
+		while (str[i])
+			i++;
+		while (str[i] == ' ' || str[i] == '\t' || str[i] == '\0')
+			i--;
+		lwe = i + 1;
+		while ( i >= 0 && (str[i] != ' ' && str[i] != '\t'))
+			i--;
+		lws = i + 1;
+		ft_putword(str, lws, lwe);
 	}
 	write(1, "\n", 1);
-	return(0);
+	return (0);
 }
