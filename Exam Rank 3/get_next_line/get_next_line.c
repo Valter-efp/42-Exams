265e361b253edfa04b86c04fc8e111298a41a0ee
Valter-efp <154928977+Valter-efp@users.noreply.github.com>
2024-09-28 16:00:56 +0100

    Update get_next_line.c

--- Exam Rank 3/get_next_line/get_next_line.c
+++ Exam Rank 3/get_next_line/get_next_line.c
@@ -1,21 +1,20 @@
-# include <unistd.h>
-# include <stdlib.h>	
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 # include <fcntl.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
 # endif
 
-char	*ft_strdup(char *src)
+char *ft_strdup(char *src)
 {
 	char	*dest;
-	int		i;
+	int		i = 0;
 
-	i = 0;
 	while (src[i])
 		i++;
-	dest = (char *)malloc(sizeof(char) * (i + 1));
+	dest = malloc(sizeof(char) * (i +1));
 	i = 0;
 	while (src[i])
 	{
@@ -26,15 +25,14 @@ char	*ft_strdup(char *src)
 	return (dest);
 }
 
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE];
 	char		line[70000];
 	static int	buffer_read;
-	static int 	buffer_pos;
-	int			i;
+	static int	buffer_pos;
+	int			i = 0;
 
-	i = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	while (1)
@@ -44,14 +42,14 @@ char	*get_next_line(int fd)
 			buffer_read = read(fd, buffer, BUFFER_SIZE);
 			buffer_pos = 0;
 			if (buffer_read <= 0)
-				break ;
+				break;
 		}
 		line[i++] = buffer[buffer_pos++];
-		if (line[i - 1] == '\n')
-			break ;
+		if (line[i -1] == '\n')
+			break;
 	}
 	line[i] = '\0';
 	if (i == 0)
 		return (NULL);
-	return (ft_strdup(line));
+	return(ft_strdup(line));
 }
