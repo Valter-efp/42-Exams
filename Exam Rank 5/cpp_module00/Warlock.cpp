2f72935f60cdf8c20cb1d86ff071f510b64e7981
Valter-efp <154928977+Valter-efp@users.noreply.github.com>
2025-07-05 10:49:53 +0100

    Update Warlock.cpp

--- Exam Rank 5/cpp_module00/Warlock.cpp
+++ Exam Rank 5/cpp_module00/Warlock.cpp
@@ -1,5 +1,15 @@
 #include "Warlock.hpp"
 
+Warlock::Warlock(const std::string &name, const std::string &title): _name(name), _title(title)
+{
+    std::cout << getName() << ": This looks like another boring day." << std::endl;
+}
+
+Warlock::~Warlock()
+{
+    std::cout << getName() << ": My job here is done!" << std::endl;
+}
+
 const std::string &Warlock::getName() const
 {
     return (this->_name);
@@ -18,14 +28,4 @@ void Warlock::setTitle(const std::string &title)
 void Warlock::introduce() const
 {
     std::cout << getName() << ": I am " << getName() << ", " << getTitle() << "!" << std::endl;
-}
-
-Warlock::Warlock(const std::string &name, const std::string &title): _name(name), _title(title)
-{
-    std::cout << getName() << ": This looks like another boring day." << std::endl;
-}
-
-Warlock::~Warlock()
-{
-    std::cout << getName() << ": My job here is done!" << std::endl;
 }
